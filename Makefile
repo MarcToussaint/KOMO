073f225e0e4736ad7d9124cb1e0d1c61603bb0ad
Marc Toussaint <marc.toussaint@informatik.uni-stuttgart.de>
2018-03-21 18:09:14 -0400

    updated with newest rai

--- Makefile
+++ Makefile
@@ -4,13 +4,15 @@ DEPEND = KOMO Core Algo Geo Kin Gui Optim
 
 test_paths = $(shell find test -maxdepth 3 -name 'Makefile' -printf "%h ")
 
-build: $(DEPEND:%=makeDepend/%) $(test_paths:%=makePath/%)
+build: $(DEPEND:%=inPath_makeLib/%) $(test_paths:%=inPath_make/%)
 
-initUbuntuPackages: $(DEPEND:%=initUbuntuPackages/%)
+initUbuntuPackages: $(DEPEND:%=inPath_installUbuntu/%)
 
-clean: $(DEPEND:%=cleanPath/%) $(test_paths:%=cleanPath/%)$
+printUbuntu: $(DEPEND:%=inPath_printUbuntuPackages/%) printUbuntuPackages
 
-runTests: $(test_paths:%=makePath/%) $(test_paths:%=runPath/%)
+clean: $(DEPEND:%=inPath_clean/%) $(test_paths:%=inPath_clean/%)$
+
+runTests: $(test_paths:%=inPath_make/%) $(test_paths:%=inPath_run/%)
 
 include $(BASE)/build/generic.mk
 
--- test/komo/main.cpp
+++ test/komo/main.cpp
@@ -6,7 +6,7 @@
 //===========================================================================
 
 void TEST(Easy){
-  mlr::KinematicWorld K("arm.g");
+  rai::KinematicWorld K("arm.g");
   cout <<"configuration space dim=" <<K.q.N <<endl;
   KOMO komo;
   komo.setModel(K);
@@ -28,7 +28,7 @@ void TEST(Easy){
 //===========================================================================
 
 void TEST(Align){
-  mlr::KinematicWorld K("arm.g");
+  rai::KinematicWorld K("arm.g");
   cout <<"configuration space dim=" <<K.q.N <<endl;
   KOMO komo;
   komo.setModel(K);
@@ -50,14 +50,14 @@ void TEST(Align){
 
 void TEST(PR2){
   //NOTE: this uses a 25-DOF whole-body-motion model of thbe PR2
-  mlr::KinematicWorld K("model.g");
+  rai::KinematicWorld K("model.g");
   K.optimizeTree();
   makeConvexHulls(K.frames);
   K.calc_fwdPropagateFrames();
   cout <<"configuration space dim=" <<K.q.N <<endl;
-  double rand = mlr::getParameter<double>("KOMO/moveTo/randomizeInitialPose", .0);
+  double rand = rai::getParameter<double>("KOMO/moveTo/randomizeInitialPose", .0);
   if(rand){
-    rnd.seed(mlr::getParameter<uint>("rndSeed", 0));
+    rnd.seed(rai::getParameter<uint>("rndSeed", 0));
     rndGauss(K.q,rand,true);
     K.setJointState(K.q);
   }
@@ -80,7 +80,7 @@ void TEST(PR2){
 //===========================================================================
 
 void TEST(FinalPosePR2){
-  mlr::KinematicWorld K("model.g");
+  rai::KinematicWorld K("model.g");
   K.optimizeTree();
   makeConvexHulls(K.frames);
   cout <<"configuration space dim=" <<K.q.N <<endl;
@@ -92,7 +92,7 @@ void TEST(FinalPosePR2){
 //===========================================================================
 
 int main(int argc,char** argv){
-  mlr::initCmdLine(argc,argv);
+  rai::initCmdLine(argc,argv);
 
   testEasy();
   testAlign();
